d01dfdfc0d2e42c722e590926193cdea2d7c1af7
Zaid Hasan <szh619@gmail.com>
2025-04-27 12:57:41 +0300

    Added Pixel data directory + Parsing and displaying json implementation

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,37 @@
-function test(): void {
-  console.log("Hello World");
+import { promises as fs } from "fs";
+
+// Interface for the Pixel JSON file
+interface Tag {
+  type: string;
+  entries: string[];
 }
 
-test();
+interface Pixel {
+  date: string;
+  type: string;
+  scores: number[];
+  notes: string;
+  tags: Tag[];
+}
+
+async function searchPixels(): Promise<void> {
+  try {
+    // Reading the Pixel JSON file
+    // Replace the filename 'PIXELS_BACKUP.json' with the Pixel JSON file you stored in the data folder
+    const data: string = await fs.readFile(
+      "./data/PIXELS_BACKUP.json",
+      "utf-8"
+    );
+
+    // Parsing the Pixel data
+    const jsonData: Pixel[] = JSON.parse(data);
+
+    // Displaying the Pixel date
+    const result = jsonData;
+    console.log(result);
+  } catch (err) {
+    console.error("Error searching pixels:", err);
+  }
+}
+
+searchPixels();
